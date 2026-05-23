dd80d50d9ce657812abcc90ae36e4b52d8343133
Artem Ivanov <i@artyom.space>
2026-05-23 16:47:17 +0300

    open in windowed mode by default
    User can still toggle fullscreen via the green button or
    Cmd+Ctrl+F. Removes the auto-fullscreen onAppear and the
    contentSize resizability constraint so the window can be
    freely resized.

--- WokyisScreensaver/WokyisScreensaverApp.swift
+++ WokyisScreensaver/WokyisScreensaverApp.swift
@@ -1,18 +1,11 @@
 import SwiftUI
-import AppKit
 
 @main
 struct WokyisScreensaverApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onAppear {
-                    DispatchQueue.main.async {
-                        NSApp.windows.first?.toggleFullScreen(nil)
-                    }
-                }
         }
         .windowStyle(.hiddenTitleBar)
-        .windowResizability(.contentSize)
     }
 }
