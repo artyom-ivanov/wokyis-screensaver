97bd0afa07a2b5f27e8e79fca9a3ab2308d355cf
Artem Ivanov <i@artyom.space>
2026-05-23 15:40:49 +0300

    scaffold SwiftUI app + xcodegen project

---
+++ WokyisScreensaver/WokyisScreensaverApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct WokyisScreensaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+    }
+}
